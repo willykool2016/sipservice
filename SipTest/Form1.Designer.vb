4ae0f5012ac9669a23b7d2ad440a24c02bf8a985
grobertson-png <g.robertson@tmtsolutions.com>
2026-07-13 11:34:58 -0500

    first commit

---
+++ SipTest/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnConnect = New Button()
+        btnDisconnect = New Button()
+        lblStatus = New Label()
+        txtLog = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnConnect
+        ' 
+        btnConnect.Location = New Point(137, 153)
+        btnConnect.Name = "btnConnect"
+        btnConnect.Size = New Size(100, 183)
+        btnConnect.TabIndex = 0
+        btnConnect.Text = "btnConnect"
+        btnConnect.UseVisualStyleBackColor = True
+        ' 
+        ' btnDisconnect
+        ' 
+        btnDisconnect.Location = New Point(524, 152)
+        btnDisconnect.Name = "btnDisconnect"
+        btnDisconnect.Size = New Size(108, 193)
+        btnDisconnect.TabIndex = 1
+        btnDisconnect.Text = "btnDisconnect"
+        btnDisconnect.UseVisualStyleBackColor = True
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.AutoSize = True
+        lblStatus.Location = New Point(298, 61)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(36, 20)
+        lblStatus.TabIndex = 2
+        lblStatus.Text = "N/A"
+        ' 
+        ' txtLog
+        ' 
+        txtLog.Location = New Point(240, 102)
+        txtLog.Name = "txtLog"
+        txtLog.Size = New Size(240, 27)
+        txtLog.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtLog)
+        Controls.Add(lblStatus)
+        Controls.Add(btnDisconnect)
+        Controls.Add(btnConnect)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents btnDisconnect As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents txtLog As TextBox
+
+End Class
---
+++ SipTest/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Text
+
+
+Imports System.Net.WebSockets
+Imports System.Threading
+'Imports System.Text
+'Imports System.Net.Http
+
+
+
+Public Class Form1
+    Dim client As New HttpClient()
+
+
+
+    Private webSocket As ClientWebSocket
+    Private cts As CancellationTokenSource
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim username As String = "willTestCam"
+        Dim password As String = "root"
+        Dim credentials As String = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"))
+        MessageBox.Show(credentials)
+
+        client.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue("Basic", credentials)
+        MessageBox.Show(client.ToString)
+    End Sub
+
+    Private Async Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+        cts = New CancellationTokenSource()
+        webSocket = New ClientWebSocket()
+        MessageBox.Show(client.ToString)
+        Dim uriString As String = "wss://192.168.0.195/vapix/" & client.ToString
+        Dim serverUri As New Uri(uriString)
+
+
+        Try
+
+            Await webSocket.ConnectAsync(serverUri, cts.Token)
+            lblStatus.Text = "Connected"
+
+            Await ListenForMessagesAsync(webSocket, cts.Token)
+
+
+
+        Catch ex As Exception
+
+            MessageBox.Show($"Connection Error: {ex.Message}")
+
+        End Try
+
+    End Sub
+
+
+    Private Async Function ListenForMessagesAsync(socket As ClientWebSocket, ct As CancellationToken) As Task
+
+        Dim buffer As Byte() = New Byte(1023) {}
+
+        While socket.State = WebSocketState.Open
+
+            Dim result = Await socket.ReceiveAsync(New ArraySegment(Of Byte)(buffer), ct)
+
+            If result.MessageType = WebSocketMessageType.Close Then
+
+                Await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None)
+                Exit While
+
+            End If
+
+            Dim messageString As String = Encoding.UTF8.GetString(buffer, 0, result.Count)
+
+            Me.Invoke(Sub()
+                          txtLog.AppendText(messageString & vbCrLf)
+                      End Sub)
+
+        End While
+
+    End Function
+
+    Private Async Sub btnDisconnect_Click(sender As Object, e As EventArgs) Handles btnDisconnect.Click
+
+        If webSocket IsNot Nothing AndAlso webSocket.State = WebSocketState.Open Then
+
+            cts.Cancel()
+            Await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "User disconnected", CancellationToken.None)
+            lblStatus.Text = "Disconnected"
+
+        End If
+
+    End Sub
+
+
+End Class

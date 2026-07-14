56c1e6fcff4fb325ccc03d00c25861ec8b811bdd
grobertson-png <g.robertson@tmtsolutions.com>
2026-07-14 11:15:25 -0500

    hangup and make call button added

--- SipTest/Form1.Designer.vb
+++ SipTest/Form1.Designer.vb
@@ -22,68 +22,78 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        btnDisconnect = New Button()
+        btnHangUp = New Button()
         lblStatus = New Label()
         txtLog = New TextBox()
         btnAnswer = New Button()
+        btnMakeCall = New Button()
         SuspendLayout()
         ' 
-        ' btnDisconnect
+        ' btnHangUp
         ' 
-        btnDisconnect.Location = New Point(210, 114)
-        btnDisconnect.Margin = New Padding(3, 2, 3, 2)
-        btnDisconnect.Name = "btnDisconnect"
-        btnDisconnect.Size = New Size(94, 137)
-        btnDisconnect.TabIndex = 1
-        btnDisconnect.Text = "btnDisconnect"
-        btnDisconnect.UseVisualStyleBackColor = True
+        btnHangUp.Location = New Point(173, 154)
+        btnHangUp.Name = "btnHangUp"
+        btnHangUp.Size = New Size(107, 183)
+        btnHangUp.TabIndex = 1
+        btnHangUp.Text = "HangUp"
+        btnHangUp.UseVisualStyleBackColor = True
         ' 
         ' lblStatus
         ' 
         lblStatus.AutoSize = True
-        lblStatus.Location = New Point(279, 50)
+        lblStatus.Location = New Point(319, 67)
         lblStatus.Name = "lblStatus"
-        lblStatus.Size = New Size(64, 15)
+        lblStatus.Size = New Size(81, 20)
         lblStatus.TabIndex = 2
         lblStatus.Text = "Status: idle"
         ' 
         ' txtLog
         ' 
-        txtLog.Location = New Point(210, 76)
-        txtLog.Margin = New Padding(3, 2, 3, 2)
+        txtLog.Location = New Point(240, 101)
         txtLog.Name = "txtLog"
         txtLog.PlaceholderText = "Console Status"
-        txtLog.Size = New Size(210, 23)
+        txtLog.Size = New Size(239, 27)
         txtLog.TabIndex = 3
         ' 
         ' btnAnswer
         ' 
         btnAnswer.Enabled = False
-        btnAnswer.Location = New Point(326, 114)
+        btnAnswer.Location = New Point(306, 154)
+        btnAnswer.Margin = New Padding(3, 4, 3, 4)
         btnAnswer.Name = "btnAnswer"
-        btnAnswer.Size = New Size(94, 137)
+        btnAnswer.Size = New Size(107, 183)
         btnAnswer.TabIndex = 4
         btnAnswer.Text = "Answer Call"
         btnAnswer.UseVisualStyleBackColor = True
         ' 
+        ' btnMakeCall
+        ' 
+        btnMakeCall.Location = New Point(438, 154)
+        btnMakeCall.Name = "btnMakeCall"
+        btnMakeCall.Size = New Size(107, 183)
+        btnMakeCall.TabIndex = 5
+        btnMakeCall.Text = "Make Call"
+        btnMakeCall.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(700, 338)
+        ClientSize = New Size(800, 451)
+        Controls.Add(btnMakeCall)
         Controls.Add(btnAnswer)
         Controls.Add(txtLog)
         Controls.Add(lblStatus)
-        Controls.Add(btnDisconnect)
-        Margin = New Padding(3, 2, 3, 2)
+        Controls.Add(btnHangUp)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
         PerformLayout()
     End Sub
-    Friend WithEvents btnDisconnect As Button
+    Friend WithEvents btnHangUp As Button
     Friend WithEvents lblStatus As Label
     Friend WithEvents txtLog As TextBox
     Friend WithEvents btnAnswer As Button
+    Friend WithEvents btnMakeCall As Button
 
 End Class
--- SipTest/Form1.vb
+++ SipTest/Form1.vb
@@ -30,32 +30,25 @@ Public Class Form1
 
     Private activeCallAgent As SIPUserAgent
     Private activeServerAgent As SIPServerUserAgent
+
     Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' 1. Start the SIP Server to listen for incoming calls right away
         StartListening()
-
         ' 2. Connect the WebSocket to send API commands
         Try
             lblStatus.Text = "Status: Connecting..."
-
             webSocket = New ClientWebSocket()
             cts = New CancellationTokenSource()
-
             ' Apply certificate bypass and credentials
             webSocket.Options.RemoteCertificateValidationCallback = AddressOf AcceptAllCertificates
-
             ' Make sure to put your actual device password here!
             webSocket.Options.Credentials = New System.Net.NetworkCredential("willTestCam", "root")
-
             Dim deviceIp As String = "192.168.0.208"
             Dim serverUri As New Uri($"wss://{deviceIp}/vapix/intercomws")
-
             ' Connect!
             Await webSocket.ConnectAsync(serverUri, cts.Token)
-
             lblStatus.Text = "Status: Connected & Listening"
             lblStatus.ForeColor = Color.Green
-
         Catch ex As Exception
             lblStatus.Text = "Status: Connection Failed"
             lblStatus.ForeColor = Color.Red
@@ -75,45 +68,36 @@ Public Class Form1
         Try
             ' 1. Set up the SIP Transport (listens for SIP traffic)
             sipTransport = New SIPTransport()
-
             ' 2. Set up the Windows Audio Endpoint (Mic and Speakers)
             windowsAudio = New WindowsAudioEndPoint(New SIPSorcery.Media.AudioEncoder())
-
             ' 3. Create the VoIP Media Session
             Dim voipMediaSession As New VoIPMediaSession(windowsAudio.ToMediaEndPoints())
-
             ' 4. Create the SIP User Agent (Your Softphone)
             userAgent = New SIPUserAgent(sipTransport, Nothing)
-
             ' --- Event Handlers to track the call state ---
             AddHandler userAgent.ClientCallRinging, Sub(ua, res)
                                                         Console.WriteLine("Intercom is ringing...")
                                                     End Sub
-
             AddHandler userAgent.ClientCallAnswered, Sub(ua, res)
                                                          Console.WriteLine("Call answered! Audio streaming started.")
                                                      End Sub
-
             AddHandler userAgent.ClientCallFailed, Sub(ua, err, res)
                                                        Console.WriteLine($"Call failed: {err}")
                                                    End Sub
             ' ----------------------------------------------
-
             ' 5. The Intercom's SIP Address
             ' Format: sip:username@ip_address
             Dim intercomSipUri As String = "sip:192.168.0.208:5060"
-
             ' 6. Start the Call!
             ' We pass the SIP URI and the Media Session to handle the audio
             Dim callTask = userAgent.Call(intercomSipUri, Nothing, Nothing, voipMediaSession)
-
             Await callTask
-
         Catch ex As Exception
             MessageBox.Show($"SIP Error: {ex.Message}")
         End Try
     End Function
 
+    'Where the code to make the hang up button and form closing hangup occur
     Public Sub HangUp()
         If userAgent IsNot Nothing Then
             userAgent.Hangup()
@@ -127,19 +111,15 @@ Public Class Form1
         Try
             ' 1. Set up the SIP Transport
             sipTransport = New SIPTransport()
-
             ' --- THE MISSING PIECE ---
             ' Explicitly tell SIPSorcery to open and listen on UDP Port 5060
             Dim sipChannel As New SIPUDPChannel(New Net.IPEndPoint(Net.IPAddress.Any, 5060))
             sipTransport.AddSIPChannel(sipChannel)
             ' -------------------------
-
             ' 2. Create the SIP User Agent (Your Softphone)
             userAgent = New SIPUserAgent(sipTransport, Nothing)
-
             ' 3. Tell the agent what to do when an incoming call arrives
             AddHandler userAgent.OnIncomingCall, AddressOf Intercom_Ringing
-
         Catch ex As Exception
             MessageBox.Show($"Failed to start listening: {ex.Message}")
         End Try
@@ -169,19 +149,15 @@ Public Class Form1
     Private Async Sub btnAnswer_Click(sender As Object, e As EventArgs) Handles btnAnswer.Click
         ' Safety check
         If activeCallAgent Is Nothing OrElse activeServerAgent Is Nothing Then Return
-
         Try
             ' Lock the button so it can't be double-clicked
             btnAnswer.Enabled = False
             lblStatus.Text = "Status: Connecting Audio..."
-
             ' 1. Set up the Audio endpoints (Mic and Speakers)
-            windowsAudio = New WindowsAudioEndPoint(New SIPSorcery.Media.AudioEncoder())
-            Dim voipMediaSession As New VoIPMediaSession(windowsAudio.ToMediaEndPoints())
-
+            windowsAudio = New WindowsAudioEndPoint(New AudioEncoder)
+            Dim voipMediaSession As New VoIPMediaSession(windowsAudio.ToMediaEndPoints)
             ' 2. Formally Answer the call and open the 2-way audio!
-            Dim answered As Boolean = Await activeCallAgent.Answer(activeServerAgent, voipMediaSession)
-
+            Dim answered = Await activeCallAgent.Answer(activeServerAgent, voipMediaSession)
             If answered Then
                 lblStatus.Text = "Status: Call Active! (Audio Live)"
                 lblStatus.ForeColor = Color.Green
@@ -189,7 +165,6 @@ Public Class Form1
                 lblStatus.Text = "Status: Failed to answer call."
                 lblStatus.ForeColor = Color.Red
             End If
-
         Catch ex As Exception
             MessageBox.Show($"Error answering call: {ex.Message}")
             lblStatus.Text = "Status: Error Answering"
@@ -197,23 +172,19 @@ Public Class Form1
         End Try
     End Sub
 
-    Private Async Sub btnDisconnect_Click(sender As Object, e As EventArgs) Handles btnDisconnect.Click
-
-        If webSocket IsNot Nothing AndAlso webSocket.State = WebSocketState.Open Then
-
-            cts.Cancel()
-            'Await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "User disconnected", CancellationToken.None)
-
-            If webSocket.State = WebSocketState.Open OrElse webSocket.State = WebSocketState.CloseReceived OrElse
-                webSocket.State = WebSocketState.CloseSent Then
-                Await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None)
-            End If
-
-
-            lblStatus.Text = "Disconnected"
-            'MessageBox.Show("ajksnfsnfjdnk")
-        End If
-
+    'When pressed, should hang up the call with the intercom
+    Private Sub btnHangUp_Click(sender As Object, e As EventArgs) Handles btnHangUp.Click
+        HangUp()
     End Sub
 
+    'When pressed, should send a call out to the intercom
+
+    'Makes sure that if the user closes the app, the call is hung up if they have yet to
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        HangUp()
+    End Sub
+
+    Private Sub btnMakeCall_Click_1(sender As Object, e As EventArgs) Handles btnMakeCall.Click
+        MessageBox.Show("Hello? Hello hello!")
+    End Sub
 End Class

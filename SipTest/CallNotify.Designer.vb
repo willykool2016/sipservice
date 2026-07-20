8d13ebe51402582429a7ef0bf11deb83ac762b23
grobertson-png <g.robertson@tmtsolutions.com>
2026-07-20 09:22:36 -0500

    fixed 'on call' button logo

--- SipTest/CallNotify.Designer.vb
+++ SipTest/CallNotify.Designer.vb
@@ -22,22 +22,39 @@ Partial Class CallNotify
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CallNotify))
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(87, 75)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
         ' 
         ' CallNotify
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.Red
-        ClientSize = New Size(56, 51)
+        ClientSize = New Size(166, 152)
+        Controls.Add(PictureBox1)
         FormBorderStyle = FormBorderStyle.None
         Name = "CallNotify"
-        Opacity = 0.6R
+        Opacity = 0.85R
         ShowIcon = False
         ShowInTaskbar = False
         Text = "CallNotify"
         TopMost = True
         TransparencyKey = Color.Red
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- SipTest/CallNotify.vb
+++ SipTest/CallNotify.vb
@@ -22,115 +22,11 @@ Public Class CallNotify
         End Get
     End Property
 
-    'Testing something
-    'Private img As Image = Image.FromFile("C:\Users\Glade Robertson\Downloads\New folder\mic.png")
-
-
-    '
-
-
     Protected Overrides Sub OnPaint(e As PaintEventArgs)
         MyBase.OnPaint(e)
-        'MAKE SURE TO GET AN ACTUAL IMAGE PNG
-        'Using img As Image = Image.FromFile("C:\Users\Glade Robertson\Downloads\New folder\mic2.png")
-        'Dim img As Image = System.Drawing.Bitm.Resources.mic2
-
-
-
-        Using img As Image = My.Resources.microphone
-            e.Graphics.DrawImage(img, 0, 0, CSng(Me.ClientSize.Width / 2), CSng(Me.ClientSize.Height / 2))
-
-        End Using
-        '-------------------------------
-        'Testing something below
-
-        'e.Graphics.CompositingQuality = CompositingQuality.HighQuality
-
-        'Dim opacity As Single = 0.5F
-
-        'Dim matrixItems As Single()() = {
-        '   New Single() {1, 0, 0, 0, 0},
-        '  New Single() {0, 1, 0, 0, 0},
-        ' New Single() {0, 0, 1, 0, 0},
-        'New Single() {0, 0, 0, opacity, 0},
-        ' New Single() {0, 0, 0, 0, 1}
-        '}
-
-        '  Dim colorMatrix As New ColorMatrix(matrixItems)
-
-        '  Using attributes As New ImageAttributes()
-
-        '  attributes.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap)
-
-        '  Dim destRect As New Rectangle(50, 50, img.Width, img.Height)
-
-
-        'e.Graphics.DrawImage(img, destRect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, attributes)
-        ' e.Graphics.DrawImage(img, 0, 0, CSng(Me.ClientSize.Width / 2), CSng(Me.ClientSize.Height / 2))
-        'End Using
-
     End Sub
-
-
-
-
-    'All stuff for the image #manifesting good things
-
-
-    'Private formImage As Image
-
-    'Public Sub New()
-
-    '    ' This call is required by the designer.
-    '    InitializeComponent()
-
-    '    ' Add any initialization after the InitializeComponent() call.
-
-
-    '    Me.DoubleBuffered = True
-
-    '    Me.ResizeRedraw = True
-
-    'End Sub
-
-
-
-
 
     Private Sub CallNotify_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
-        '    formImage = Image.FromFile("C:\Users\Glade Robertson\Downloads\New folder\mic.png")
-
-
     End Sub
-
-    'Private Sub CallNotify_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Load
-
-    '    If formImage IsNot Nothing Then
-
-    '        Dim opacity As Single = 0.5F
-
-    '        Dim matrix As New ColorMatrix()
-    '        matrix.Matrix33 = opacity
-
-    '        Dim attributes As New ImageAttributes()
-    '        attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap)
-
-    '        Dim destRect As New Rectangle(0, 0, CSng(Me.ClientSize.Width / 2), CSng(Me.ClientSize.Height / 2))
-
-    '        e.Graphics.DrawImage(formImage, destRect, 0, 0, formImage.Width, formImage.Height, GraphicsUnit.Pixel, attributes)
-
-
-    '    End If
-
-    'End Sub
-
-
-
-
-
-
-
 
 End Class
